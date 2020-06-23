f5c262beac5fdd7ef134ad931ae96cf2f0f57335
ThomasPeruch <peruchthomas@gmail.com>
2020-06-23 13:00:52 -0300

    Update data

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -13,28 +10,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		Connection conn = null;
 		PreparedStatement pst = null;
 		try {
 			conn=DB.getConnection();
 			
-			pst = conn.prepareStatement("insert into department(Name) values ('D1'),('D2')",
-					Statement.RETURN_GENERATED_KEYS);
-		
-			int rowsAffected = pst.executeUpdate();
+			pst = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? "
+					+ "WHERE (DepartmentId = ?)");
 			
-			if(rowsAffected > 0) {
-				ResultSet rs = pst.getGeneratedKeys();
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
+			pst.setDouble(1, 200.0);
+			pst.setInt(2, 2);
+			
+			int rowsAffected = pst.executeUpdate();
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
