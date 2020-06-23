2840da398c5762ef3580891c635a3c1abccfc17c
ThomasPeruch <peruchthomas@gmail.com>
2020-06-23 12:31:30 -0300

    Another way to insert data and return ID form inserted object

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
@@ -21,15 +20,8 @@ public class Program {
 		try {
 			conn=DB.getConnection();
 			
-			pst = conn.prepareStatement(
-					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALUES (?, ?, ?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
-		
-			pst.setString(1, "Carl Johnson");
-			pst.setString(2, "cj@gmail.com");
-			pst.setDate(3, new java.sql.Date(sdf.parse("08/04/1968").getTime()));
-			pst.setDouble(4, 3000.0);
-			pst.setInt(5, 4);
+			pst = conn.prepareStatement("insert into department(Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
 		
 			int rowsAffected = pst.executeUpdate();
 			
@@ -45,9 +37,6 @@ public class Program {
 			}
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
