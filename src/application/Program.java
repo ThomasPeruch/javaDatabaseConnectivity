34a20738b75a6dcb4a4632fa693b5704c4af8c14
ThomasPeruch <peruchthomas@gmail.com>
2020-06-23 13:10:46 -0300

    delete data

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -15,17 +16,14 @@ public class Program {
 		try {
 			conn=DB.getConnection();
 			
-			pst = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? "
-					+ "WHERE (DepartmentId = ?)");
-			
-			pst.setDouble(1, 200.0);
-			pst.setInt(2, 2);
+			pst = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			pst.setInt(1, 5);
 			
 			int rowsAffected = pst.executeUpdate();
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(pst);
